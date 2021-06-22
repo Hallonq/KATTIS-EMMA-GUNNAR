4c7a981ea84b7645ca2fffd8fc21a39337bee9c0
Ludwig <ludde-j@hotmail.com>
2021-06-22 20:05:14 +0200

    Add project files.

---
+++ YabsProblemA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace YabsProblemA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] gunnarArray = input.Split(' ');
+
+            string input2 = Console.ReadLine();
+            string[] emmaArray = input2.Split(' ');
+
+            double gunnarEV, emmaEV;
+
+            gunnarEV = ((int.Parse(gunnarArray[0]) + int.Parse(gunnarArray[1])) / 2.0) + ((int.Parse(gunnarArray[2]) + int.Parse(gunnarArray[3])) / 2.0);
+            emmaEV = ((int.Parse(emmaArray[0]) + int.Parse(emmaArray[1])) / 2.0) + ((int.Parse(emmaArray[2]) + int.Parse(emmaArray[3])) / 2.0);
+
+            if (gunnarEV > emmaEV)
+            {
+                Console.WriteLine("Gunnar");
+            }
+            else if (gunnarEV < emmaEV)
+            {
+                Console.WriteLine("Emma");
+            }
+            else
+            {
+                Console.WriteLine("Tie");
+            }
+        }
+    }
+}
---
+++ YabsProblemB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace YabsProblemB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 2000; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                string dwa = "";
+            }
+        }
+    }
+}
